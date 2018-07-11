b97f577600793ee042cde830bf62a5c2e7954e51
dattv <datthangvan@gmail.com>
2018-07-11 17:39:07 +0900

    add main program

---
+++ 2D_CARFILE.f90
@@ -0,0 +1,25 @@
+!  $2D_CARFILE.f90 
+!
+!  FUNCTIONS:
+!  $2D_CARFILE - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: $2D_CARFILE
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program $2D_CARFILE
+
+    implicit none
+
+    ! Variables
+
+    ! Body of $2D_CARFILE
+    print *, 'Hello World'
+
+    end program $2D_CARFILE
+
