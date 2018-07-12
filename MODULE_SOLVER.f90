d4142e48e25a141f0ad93c6cc628e9e88d63de5d
dattv <datthangvan@gmail.com>
2018-07-12 23:34:07 +0900

    develop solver pattern

--- MODULE_SOLVER.f90
+++ MODULE_SOLVER.f90
@@ -9,6 +9,8 @@ MODULE MODULE_SOLVER
     
     use MODULE_PRECISION
     
+!================== DEGINS PATTERN ==================    
+    
     type, abstract  :: abstract_solver
     contains
     procedure(generic_solver), deferred, pass(this) :: p_solver
@@ -26,6 +28,38 @@ MODULE MODULE_SOLVER
     	private
         character(len = 20)             :: c_solver_type
         integer(ip)                     :: i_solver_type
-        class(abstract_solver), pointer :: 
+        class(abstract_solver), pointer :: solver
+        
+    contains
+    procedure   :: create_solver
     end type solver_factory
+    
+    type, extends(abstract_solver)  :: first_order_explicit_solver
+    contains
+    procedure, pass(this)   :: p_solver => 1ST_EX_solver
+    end type first_order_explicit_solver
+    
+    type, extends(abstract_solver)  :: second_order_explicit_solver
+    contains
+    procedure, pass(this)   :: p_solver => 2ST_EX_solver
+    end type second_order_explicit_solver
+    
+    type, extends(abstract_solver)  :: runge_kutta_explicit_solver
+    contains
+    procedure, pass(this)   :: p_solver => RK_EX_solver
+    end type runge_kutta_explicit_solver
+    
+    type, extends(abstract_solver)  :: second_order_implicit_solver
+    contains
+    procedure, pass(this)   :: p_solver => 2ST_IM_solver
+    end type second_order_implicit_solver
+    
+    type, extends(abstract_solver)  :: implicit_Explicit_solver
+    contains
+    procedure, pass(this)   :: p_solver => IMEX_solver
+    end type implicit_Explicit_solver
+    
+!=========================== END OF FACTORY PATTERN ==========================    
+    contains
+    
 END MODULE MODULE_SOLVER    
